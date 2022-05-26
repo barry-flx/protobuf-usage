10c5b05fc215b7874f1a527d59a7bf10c169cae0
fuzhengtao <fztjavafans@163.com>
2022-05-26 10:16:45 +0800

    debug course code

--- src/test/java/com/flx/protobuf/TestCourse.java
+++ src/test/java/com/flx/protobuf/TestCourse.java
@@ -1,8 +1,18 @@
 package com.flx.protobuf;
 
+import com.flx.protobuf.dto.Course;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCourse {
 
-
+  @Test
+  public void testCourse() {
+    Course course = Course.newBuilder().setOrderNo("1234567890abc").setScore(1).build();
+    String str = course.toString();
+    Assert.assertNotNull(str);
+    System.out.println(str);
+  }
 
 
 }
