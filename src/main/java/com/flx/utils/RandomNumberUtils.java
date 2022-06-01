ee004afc795c5c364064fcb4b959baba786d8b2c
fuzhengtao <fztjavafans@163.com>
2022-06-01 09:37:00 +0800

    add random number method

---
+++ src/main/java/com/flx/utils/RandomNumberUtils.java
@@ -0,0 +1,25 @@
+package com.flx.utils;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class RandomNumberUtils {
+
+  private static final String BASE_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  private RandomNumberUtils() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  public static String getRandomString(int length) throws NoSuchAlgorithmException {
+    Random random = SecureRandom.getInstanceStrong();
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      int number = random.nextInt(BASE_STR.length());
+      builder.append(BASE_STR.charAt(number));
+    }
+    return builder.toString();
+  }
+
+}
